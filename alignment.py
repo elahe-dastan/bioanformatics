33877323ac467fc0dea96b1368c21da918dc59f1
Elahe Dastan <elahe.dstn@gmail.com>
2021-03-27 19:48:08 +0430

    print matrix

--- alignment.py
+++ alignment.py
@@ -3,27 +3,38 @@ from scoring_matrix import *
 
 
 class Alignment:
-    def __init__(self, gap_penalty: int):
+    def __init__(self, gap_penalty: int, a: str, b: str):
         self.scoring_matrix = PAM250
         self.gap_penalty = gap_penalty
-
-    def local_alignment(self, a: str, b: str):
+        self.a = a
+        self.b = b
         rows, cols = (len(a) + 1, len(b) + 1)
-        matrix = np.zeros((rows, cols))
-        matrix = self.initialization(matrix)
-        matrix = self.fill_matrix(matrix)
-        print(matrix)
+        self.matrix = np.zeros((rows, cols))
 
-    def initialization(self, matrix: np.array) -> np.array:
-        matrix[0][0] = 0
-        for i in range(1, len(matrix)):
-            matrix[i][0] = -5 * i
-        for i in range(1, len(matrix[0])):
-            matrix[0][i] = -5 * i
-        return matrix
+    def local_alignment(self):
+        self.initialization()
+        self.fill_matrix()
+        print(self.matrix)
 
-    def fill_matrix(self, matrix: np.array) -> np.array:
-        for i in range(1, len(matrix)):
-            for j in range(1, len(matrix[0])):
-                matrix[i][j] = score
-        return matrix
+    def initialization(self):
+        self.matrix[0][0] = 0
+        for i in range(1, len(self.matrix)):
+            self.matrix[i][0] = -5 * i
+        for i in range(1, len(self.matrix[0])):
+            self.matrix[0][i] = -5 * i
+
+    def fill_matrix(self):
+        for i in range(1, len(self.matrix)):
+            for j in range(1, len(self.matrix[0])):
+                self.matrix[i][j] = self.score(i, j)
+
+    def score(self, i: int, j: int) -> int:
+        s = self.scoring_matrix[self.a[i - 1]][self.b[j - 1]] # match/mismatch score
+        m = self.matrix[i - 1][j - 1] + s # m stands for max
+        gap = self.matrix[i-1][j] + self.gap_penalty
+        if gap > m:
+            m = gap
+        gap = self.matrix[i][j-1] + self.gap_penalty
+        if gap > m:
+            m = gap
+        return m
--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from alignment import *
 A = input("Enter first string: ")
 B = input("Enter second string: ")
 
-ali = Alignment(-5)
-ali.local_alignment(A, B)
+ali = Alignment(-5, A, B)
+ali.local_alignment()
